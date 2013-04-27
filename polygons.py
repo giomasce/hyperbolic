12992ad426342023e034d97de6c7418149435273
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2013-04-27 11:37:36 +0200

    Inverse side and angle functions for polygons.

--- polygons.py
+++ polygons.py
@@ -3,18 +3,43 @@
 
 import sys
 import mpmath as math
+from scipy import optimize
 
 from hyperbolic import Point, PointedVector, Line
 
-def get_angle_side(num, radius):
+MAX_SEARCH = 100.0
+
+def get_angle_from_radius(num, radius):
     origin = PointedVector(0.0, 0.0, 0.0)
     p1 = origin.advance(radius)
     p2 = origin.turn(2 * math.pi / float(num)).advance(radius)
     ray_line = origin.to_line().reverse()
     side_line = p1.to_point().line_to(p2.to_point())
     angle = ray_line.angle_with(side_line, intersection=p1)
-    side = p1.to_point().distance(p2, line=side_line)
-    return 2.0 * angle, side
+    return 2.0 * angle
+
+def get_side_from_radius(num, radius):
+    origin = PointedVector(0.0, 0.0, 0.0)
+    p1 = origin.advance(radius)
+    p2 = origin.turn(2 * math.pi / float(num)).advance(radius)
+    side = p1.to_point().distance(p2.to_point())
+    return side
+
+def get_radius_from_side(num, side):
+    func = lambda x: get_side_from_radius(num, x) - side
+    radius, root_results = optimize.brentq(func, 0.0, MAX_SEARCH, full_output=True)
+    if root_results.converged:
+        return radius
+    else:
+        raise Exception("Could not compute radius")
+
+def get_radius_from_angle(num, angle):
+    func = lambda x: get_angle_from_radius(num, x) - angle
+    radius, root_results = optimize.brentq(func, 0.0, MAX_SEARCH, full_output=True)
+    if root_results.converged:
+        return radius
+    else:
+        raise Exception("Could not compute radius")
 
 def build_polygon_with_center(num, radius, pv):
     return [pv.turn(2 * math.pi * float(k) / float(num)).advance(radius).to_point() for k in xrange(num)]
